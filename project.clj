e100aca4d906dcf8268f53142529741054d705e8
Jozef Wagner <jozef.wagner@gmail.com>
2013-07-18 23:15:49 +0200

    remove warn on reflections

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as ClojureScript"}
-  :warn-on-reflection true)
+            :comments "same as ClojureScript"})
