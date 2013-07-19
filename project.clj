7e5d221a60caecb34efa414cf73f5fe96e5bd92c
Jozef Wagner <jozef.wagner@gmail.com>
2013-07-19 16:07:16 +0200

    minor

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.wagjo/data-cljs "0.1.0-SNAPSHOT"
   :description "Finger trees and miscellaneous functions for ClojureScript data structures."
   :url "https://github.com/wagjo/data-cljs"
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src/cljs"]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
