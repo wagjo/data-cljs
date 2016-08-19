37f963eb5183cc441c5cfda1c8ea2b89f06b50e2
Jozef Wagner <jozef.wagner@gmail.com>
2016-08-19 23:14:15 +0200

    Prepare next version

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject com.wagjo/data-cljs "0.1.0-SNAPSHOT"
+(defproject com.wagjo/data-cljs "0.2.1-SNAPSHOT"
   :description "Finger trees and miscellaneous functions for ClojureScript data structures."
   :url "https://github.com/wagjo/data-cljs"
+  :scm {:name "git" :url "https://github.com/wagjo/data-cljs"}
+  :signing {:gpg-key "AFA4E115"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :source-paths ["src/cljs"]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
